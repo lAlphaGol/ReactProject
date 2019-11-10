494498401f74e964a2fab31b7e3c0c864c9729d6
lAlphaGol <1259200539@qq.com>
2019-11-10 19:15:22 +0800

    2019-11-10
    初步完成基于react-amap的web端：
    与同组开发人员的服务器（express+node.js）相连，
    通过fetch对其mysql进行增删改查，并显示在web端上。

---
+++ my-react-demo/build/precache-manifest.2fd7984c9bb884ed7fcda1d7b4c5e17f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247560c5246dca771498143cbe39ac9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f53745ae61458979d33a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cba8ad2ea91a4fe2f6e0",
+    "url": "/static/js/2.2cf74f70.chunk.js"
+  },
+  {
+    "revision": "f53745ae61458979d33a",
+    "url": "/static/js/main.e97ba8b3.chunk.js"
+  },
+  {
+    "revision": "1beb244efee821bcf47c",
+    "url": "/static/js/runtime-main.5ab0df93.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
